3b7d7c0dda2bc046ed1f124dfa53c8a4876406a2
Anony <ljx1583861@qq.com>
2022-08-23 16:23:26 +0800

    修改内边距BUG

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -1,10 +1,19 @@
 <template>
-  <div>员工</div>
+  <div class="emplayees-container">
+    <PageTools :show-before="true">
+      <span slot="before">共166条数据</span>
+      <template v-slot:after>
+        <el-button size="small" type="warning">导入</el-button>
+        <el-button size="small" type="danger">导出</el-button>
+        <el-button size="small" type="primary">新增员工</el-button>
+      </template>
+    </PageTools>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'HrsaasIndex',
+  name: 'Employees',
   data() {
     return {}
   }
